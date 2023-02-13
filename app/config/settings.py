453e83a680c83fda818cd175aeefebd2f6cb4532
heidless <rob.lockhart@yahoo.co.uk>
2023-02-13 17:07:17 +0000

    fixed environ issue

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,10 +1,11 @@
-# TEST COMMENTimport environ
+import environ
 import os
 from pathlib import Path
 
 # Initialise environment variables
-env = environ.Env()
-environ.Env.read_env()
+
+env = environ.Env(DEBUG=(bool, False))
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
